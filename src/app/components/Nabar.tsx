d3be82de7b4f4e25c134367d62d5146bc6e58176
Nourdine bouya <nourdinebouya@gmail.com>
2024-10-07 21:38:01 +0100

    n3del navbar

---
+++ src/app/components/Nabar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nabar = () => {
+  return (
+    <div className='bg-white shadow-md px-20 py-6 flex justify-between'>
+        <h1>Idlahcen Ourika ..</h1>
+        <nav>
+            <ul className='flex space-x-4'>
+                <li>Home</li>
+                <li>Our Jobs</li>
+                <li>Sefrvices</li>
+                <li>Contact-Me</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Nabar
